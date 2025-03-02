e28c4e5a2fb44d0b28e0d85426372756fc520f63
kosieram21 <mitchkos21@gmail.com>
2025-03-02 14:35:25 -0600

    code cleanup

--- model/layers/SelfAttentionTransformer.py
+++ model/layers/SelfAttentionTransformer.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 class SelfAttentionTransformer(nn.Module):
     def __init__(self, token_dim, num_layers, num_heads):
         super().__init__()
-        self.d_model = token_dim
+        
         self.transformer_layers = nn.TransformerEncoderLayer(d_model=token_dim, nhead=num_heads, batch_first=True)
         self.encoder = nn.TransformerEncoder(encoder_layer=self.transformer_layers, num_layers=num_layers)
 
