0ac2f2e44ec9e5329a099545784b8f08a137278e
Mitchell Kosieradzki <kosie011@cege-u-tol-gpu-02.cege.umn.edu>
2025-03-14 17:09:52 +0000

    train on whole batch wip

--- train.py
+++ train.py
@@ -9,29 +9,29 @@ def train(dataloader, model, epochs, lr, weight_decay, gamma, device):
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=gamma)
 
     # TODO: something is wrong with the data. we need to be able to just fit a flow field for a single data sample...
-    road_map, agent_trajectories, unobserved_positions, future_times, target_velocity, target_occupancy_grid = next(iter(dataloader))
-    for epoch in range(10000):#range(epochs):
+    #road_map, agent_trajectories, unobserved_positions, future_times, target_velocity, target_occupancy_grid = next(iter(dataloader))
+    for epoch in range(epochs): #range(10000):
         epoch_loss = 0
         num_batches = 0
-        #for road_map, agent_trajectories, unobserved_positions, future_times, target_velocity, target_occupancy_grid in dataloader:
-        road_map = road_map.to(device)
-        agent_trajectories = agent_trajectories.to(device)
-        unobserved_positions = unobserved_positions.to(device)
-        future_times = future_times.to(device)
-        target_velocity = target_velocity.to(device)
-        target_occupancy_grid = target_occupancy_grid.to(device)
+        for road_map, agent_trajectories, unobserved_positions, future_times, target_velocity, target_occupancy_grid in dataloader:
+            road_map = road_map.to(device)
+            agent_trajectories = agent_trajectories.to(device)
+            unobserved_positions = unobserved_positions.to(device)
+            future_times = future_times.to(device)
+            target_velocity = target_velocity.to(device)
+            target_occupancy_grid = target_occupancy_grid.to(device)
 
-        flow = model(future_times, unobserved_positions, road_map, agent_trajectories)
-        #flow = model(future_times, unobserved_positions, None)
-        loss = F.mse_loss(flow, target_velocity)
-        print(f'Batch {num_batches+1}, Loss: {loss}')
+            flow = model(future_times, unobserved_positions, road_map, agent_trajectories)
+            #flow = model(future_times, unobserved_positions, None)
+            loss = F.mse_loss(flow, target_velocity)
+            print(f'Batch {num_batches+1}, Loss: {loss}')
 
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
 
-        epoch_loss += loss.item()
-        num_batches += 1
+            epoch_loss += loss.item()
+            num_batches += 1
         # end for
 
         scheduler.step()
