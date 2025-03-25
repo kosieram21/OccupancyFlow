346514f3decaf08d889063ecae0b42c5f7c0bb88
Mitchell Kosieradzki <kosie011@cege-u-tol-gpu-02.cege.umn.edu>
2025-03-25 19:45:20 +0000

    prepare for training run

--- experiment.py
+++ experiment.py
@@ -2,7 +2,6 @@ import torch
 from torch.utils.data import DataLoader
 from datasets.Waymo import WaymoDataset, waymo_collate_fn, create_idx
 from model import OccupancyFlowNetwork
-from model.layers.ODE import ODE # TODO: delete me
 from train import train
 
 should_index = False
@@ -19,31 +18,14 @@ dataloader = DataLoader(dataset, batch_size=1, collate_fn=lambda x: waymo_collat
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 
-occupancy_flow_net = OccupancyFlowNetwork(road_map_image_size=224, trajectory_feature_dim=9, 
+occupancy_flow_net = OccupancyFlowNetwork(road_map_image_size=224, trajectory_feature_dim=10, 
 										  motion_encoder_hidden_dim=512, motion_encoder_seq_len=11,
 										  flow_field_hidden_dim=512, flow_field_fourier_features=128,
 										  token_dim=768, embedding_dim=128).to(device)
-#flow_field = ODE(2, 0, (512 for _ in range(4)), 128).to(device)
-
-# road_map, agent_trajectories, unobserved_positions, future_times, target_velocity, target_occupancy_grid = next(iter(dataloader))
-# road_map = road_map.to(device)
-# agent_trajectories = agent_trajectories.to(device)
-# unobserved_positions = unobserved_positions.to(device)
-# future_times = future_times.to(device)
-# target_velocity = target_velocity.to(device)
-# target_occupancy_grid = target_occupancy_grid.to(device)
-
-# print(f'road map: {road_map.shape}')
-# print(f'agent trajectories: {agent_trajectories.shape}')
-# print(f'unobserved positions: {unobserved_positions.shape}')
-# print(f'future times: {future_times.shape}')
-# print(f'target velocity: {target_velocity.shape}')
-# flow = occupancy_flow_net(future_times, unobserved_positions, road_map, agent_trajectories)
-# print(f'flow: {flow.shape}')
 
 train(dataloader, 
 	  occupancy_flow_net, 
-	  epochs=1, 
+	  epochs=10, 
 	  lr=1e-3,
 	  weight_decay=0,
 	  gamma=0.999,
