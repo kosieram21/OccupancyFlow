288b8c8bc2ddfe59d2a76b3f960fa228c509b55b
Mitchell Kosieradzki <kosie011@cege-u-tol-gpu-02.cege.umn.edu>
2025-03-25 19:45:20 +0000

    prepare for training run

--- train.py
+++ train.py
@@ -8,20 +8,10 @@ def train(dataloader, model, epochs, lr, weight_decay, gamma, device):
     optim = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=gamma)
 
-    # hack: store 10 batches up front
-    batches = []
-    for i, batch in enumerate(dataloader):
-        if i >= 10:
-            break
-        batches.append(batch)
-
-    # TODO: something is wrong with the data. we need to be able to just fit a flow field for a single data sample...
-    #road_map, agent_trajectories, unobserved_positions, future_times, target_velocity, target_occupancy_grid = next(iter(dataloader))
-    for epoch in range(1000): #range(epochs):
+    for epoch in range(epochs):
         epoch_loss = 0
         num_batches = 0
-        #for road_map, agent_trajectories, unobserved_positions, future_times, target_velocity, target_occupancy_grid in dataloader:
-        for road_map, agent_trajectories, unobserved_positions, future_times, target_velocity, target_occupancy_grid in batches:
+        for road_map, agent_trajectories, unobserved_positions, future_times, target_velocity, target_occupancy_grid in dataloader:
             road_map = road_map.to(device)
             agent_trajectories = agent_trajectories.to(device)
             unobserved_positions = unobserved_positions.to(device)
@@ -30,9 +20,8 @@ def train(dataloader, model, epochs, lr, weight_decay, gamma, device):
             target_occupancy_grid = target_occupancy_grid.to(device)
 
             flow = model(future_times, unobserved_positions, road_map, agent_trajectories)
-            #flow = model(future_times, unobserved_positions, None)
             loss = F.mse_loss(flow, target_velocity)
-            #print(f'Batch {num_batches+1}, Loss: {loss}')
+            print(f'Batch {num_batches+1}, Loss: {loss}')
 
             optim.zero_grad()
             loss.backward()
@@ -40,7 +29,6 @@ def train(dataloader, model, epochs, lr, weight_decay, gamma, device):
 
             epoch_loss += loss.item()
             num_batches += 1
-        # end for
 
         scheduler.step()
         avg_loss = epoch_loss / num_batches
