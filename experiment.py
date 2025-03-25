4954cb50d9f86f870c9a0a46635e5061a71b7558
Mitchell Kosieradzki <kosie011@cege-u-tol-gpu-02.cege.umn.edu>
2025-03-25 20:05:22 +0000

    data parallelism wip

--- experiment.py
+++ experiment.py
@@ -17,13 +17,13 @@ batch_size = torch.cuda.device_count() if torch.cuda.is_available() else 1
 dataset = WaymoDataset(tfrecord_path, idx_path)
 dataloader = DataLoader(dataset, batch_size=1, collate_fn=lambda x: waymo_collate_fn(x))
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f'Detected Device: {device}')
-
 occupancy_flow_net = OccupancyFlowNetwork(road_map_image_size=224, trajectory_feature_dim=10, 
 										  motion_encoder_hidden_dim=512, motion_encoder_seq_len=11,
 										  flow_field_hidden_dim=512, flow_field_fourier_features=128,
 										  token_dim=768, embedding_dim=128)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'Detected Device: {device}')
 
 if torch.cuda.device_count() > 1: # TODO: need to be able to produce batches larger than 1 to use nn.DataParallel
 	print(f'Using data parallelism across {torch.cuda.device_count()} GPUs')
