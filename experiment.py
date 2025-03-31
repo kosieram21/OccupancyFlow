27549dca9ec956260cbe14d21cf0e70657bbc27b
Mitchell Kosieradzki <kosie011@cege-u-tol-gpu-02.cege.umn.edu>
2025-03-31 20:26:29 +0000

    clean config injection into training routine

--- experiment.py
+++ experiment.py
@@ -58,36 +58,51 @@ def single_device_train(config):
         device=device
     )
 
-def distributed_train(rank, world_size, tfrecord_path, idx_path, batch_size):
+def distributed_train(rank, world_size, config):
     dist.init_process_group(backend='nccl', rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
 
     #sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank) 
     # TODO: are we even splitting the data correctly without the sampler??
-    dataset = WaymoDataset(tfrecord_path, idx_path)
-    dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=lambda x: waymo_collate_fn(x))
+    dataset = WaymoDataset(config.tfrecord_path, config.idx_path)
+    dataloader = DataLoader(dataset, batch_size=config.batch_size, collate_fn=lambda x: waymo_collate_fn(x))
     
-    occupancy_flow_net = OccupancyFlowNetwork(road_map_image_size=224, trajectory_feature_dim=10, 
-                                              motion_encoder_hidden_dim=512, motion_encoder_seq_len=11,
-                                              flow_field_hidden_dim=512, flow_field_fourier_features=128,
-                                              token_dim=768, embedding_dim=128)
+    occupancy_flow_net = OccupancyFlowNetwork(
+        road_map_image_size=config.road_map_image_size, 
+        trajectory_feature_dim=config.trajectory_feature_dim, 
+        motion_encoder_hidden_dim=config.motion_encoder_hidden_dim, 
+        motion_encoder_seq_len=config.motion_encoder_seq_len,                                      
+        flow_field_hidden_dim=config.flow_field_hidden_dim, 
+        flow_field_fourier_features=config.flow_field_fourier_features,
+        token_dim=config.token_dim, 
+        embedding_dim=config.embedding_dim
+    )
+
     #occupancy_flow_net = torch.compile(occupancy_flow_net) #TODO: can we get compile to work and be fast?
     occupancy_flow_net.to(rank)
     occupancy_flow_net = nn.parallel.DistributedDataParallel(occupancy_flow_net, 
                                                              device_ids=[rank], 
                                                              find_unused_parameters=True)
     
-    train(dataloader, occupancy_flow_net, epochs=10, lr=1e-3, weight_decay=0, gamma=0.999, device=rank)
+    train(
+        dataloader=dataloader, 
+        model=occupancy_flow_net, 
+        epochs=config.epochs, 
+        lr=config.lr, 
+        weight_decay=config.weight_decay, 
+        gamma=config.gamma, 
+        device=rank
+    )
     
     dist.destroy_process_group()
 
-def multi_device_train(tfrecord_path, idx_path, batch_size):
+def multi_device_train(config):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
     os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'DETAIL'
 
     world_size = torch.cuda.device_count()
-    mp.spawn(distributed_train, args=(world_size, tfrecord_path, idx_path, batch_size,), nprocs=world_size, join=True)
+    mp.spawn(distributed_train, args=(world_size, config,), nprocs=world_size, join=True)
 
 if __name__ == "__main__":
     should_index = False
@@ -114,7 +129,7 @@ if __name__ == "__main__":
     if should_index:
         create_idx(config.tfrecord_path, config.idx_path)
 
-    #if torch.cuda.is_available() and data_parallel:
-    #    multi_device_train(tfrecord_path, idx_path, batch_size)
-    #else:
-    single_device_train(config)
+    if torch.cuda.is_available() and data_parallel:
+        multi_device_train(config)
+    else:
+        single_device_train(config)
