f874387687dc97651c578613ec9b7f39a1daa324
Mitchell Kosieradzki <kosie011@cege-u-tol-gpu-02.cege.umn.edu>
2025-03-31 20:03:19 +0000

    cleanup experiment script

--- experiment.py
+++ experiment.py
@@ -9,18 +9,29 @@ from datasets.Waymo import WaymoDataset, waymo_collate_fn, create_idx
 from model import OccupancyFlowNetwork
 from train import train
 
-TFRECORD_PATH = '../data1/waymo_dataset/uncompressed/tf_example/validation'
-IDX_PATH = '../idx/validation'
-BATCH_SIZE = 16
+def single_device_train(tfrecord_path, idx_path, batch_size):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def distributed_train(rank, world_size):
+    dataset = WaymoDataset(tfrecord_path, idx_path)
+    dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=lambda x: waymo_collate_fn(x))
+
+    occupancy_flow_net = OccupancyFlowNetwork(road_map_image_size=224, trajectory_feature_dim=10, 
+                                              motion_encoder_hidden_dim=512, motion_encoder_seq_len=11,
+                                              flow_field_hidden_dim=512, flow_field_fourier_features=128,
+                                              token_dim=768, embedding_dim=128)
+    #occupancy_flow_net = torch.compile(occupancy_flow_net) #TODO: can we get compile to work and be fast?
+    occupancy_flow_net = occupancy_flow_net.to(device)
+
+    train(dataloader, occupancy_flow_net, epochs=10, lr=1e-3, weight_decay=0, gamma=0.999, device=device)
+
+def distributed_train(rank, world_size, tfrecord_path, idx_path, batch_size):
     dist.init_process_group(backend='nccl', rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
 
     #sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank) 
     # TODO: are we even splitting the data correctly without the sampler??
-    dataset = WaymoDataset(TFRECORD_PATH, IDX_PATH)
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, collate_fn=lambda x: waymo_collate_fn(x))
+    dataset = WaymoDataset(tfrecord_path, idx_path)
+    dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=lambda x: waymo_collate_fn(x))
     
     occupancy_flow_net = OccupancyFlowNetwork(road_map_image_size=224, trajectory_feature_dim=10, 
                                               motion_encoder_hidden_dim=512, motion_encoder_seq_len=11,
@@ -36,37 +47,26 @@ def distributed_train(rank, world_size):
     
     dist.destroy_process_group()
 
-def multi_device_train():
+def multi_device_train(tfrecord_path, idx_path, batch_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
     os.environ['TORCH_DISTRIBUTED_DEBUG'] = 'DETAIL'
 
     world_size = torch.cuda.device_count()
-    mp.spawn(distributed_train, args=(world_size,), nprocs=world_size, join=True)
-
-def single_device_train():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    dataset = WaymoDataset(TFRECORD_PATH, IDX_PATH)
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, collate_fn=lambda x: waymo_collate_fn(x))
-
-    occupancy_flow_net = OccupancyFlowNetwork(road_map_image_size=224, trajectory_feature_dim=10, 
-                                              motion_encoder_hidden_dim=512, motion_encoder_seq_len=11,
-                                              flow_field_hidden_dim=512, flow_field_fourier_features=128,
-                                              token_dim=768, embedding_dim=128)
-    #occupancy_flow_net = torch.compile(occupancy_flow_net) #TODO: can we get compile to work and be fast?
-    occupancy_flow_net = occupancy_flow_net.to(device)
-
-    train(dataloader, occupancy_flow_net, epochs=10, lr=1e-3, weight_decay=0, gamma=0.999, device=device)
+    mp.spawn(distributed_train, args=(world_size, tfrecord_path, idx_path, batch_size,), nprocs=world_size, join=True)
 
 if __name__ == "__main__":
     should_index = False
-    data_parallel = False
+    data_parallel = True
+
+    tfrecord_path = '../data1/waymo_dataset/uncompressed/tf_example/validation'
+    idx_path = '../idx/validation'
+    batch_size = 16
 
     if should_index:
-        create_idx(TFRECORD_PATH, IDX_PATH)
+        create_idx(tfrecord_path, idx_path)
 
     if torch.cuda.is_available() and data_parallel:
-        multi_device_train()
+        multi_device_train(tfrecord_path, idx_path, batch_size)
     else:
-        single_device_train()
+        single_device_train(tfrecord_path, idx_path, batch_size)
