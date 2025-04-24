528673d382d4e633a652b77d61e3aeb518db175e
Mitchell Kosieradzki <kosie011@cege-u-tol-gpu-02.cege.umn.edu>
2025-04-24 01:42:57 +0000

    distributed tensor caching

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,15 @@
 import os
 import argparse
-from datasets import WaymoDataset, waymo_collate_fn, create_idx, cache_data
+import multiprocessing as mp
+import torch
 from torch.utils.data import DataLoader
+from datasets import WaymoDataset, waymo_collate_fn, create_idx, cache_data
+
+def worker(tfrecord_dir, idx_dir, cache_dir, worker_id, num_workers):
+    dataset = WaymoDataset(tfrecord_dir, idx_dir, worker_id, num_workers)
+    dataloader = DataLoader(dataset, batch_size=1, collate_fn=waymo_collate_fn)
+    worker_cache_dir = os.path.join(cache_dir, f"worker_{worker_id}")
+    cache_data(dataloader, worker_cache_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Preprocess the waymo dataset and cache the scene tensors for fast acess.")
@@ -15,9 +23,20 @@ if __name__ == "__main__":
     if not os.path.exists(args.tfrecord_dir):
         raise FileNotFoundError(f"tfrecord directory does not exist: {args.tfrecord_dir}")
 
-    create_idx(args.tfrecord_dir, args.idx_dir)
+    #create_idx(args.tfrecord_dir, args.idx_dir)
 
-    dataset = WaymoDataset(args.tfrecord_dir, args.idx_dir)
-    dataloader = DataLoader(dataset, batch_size=1, collate_fn=waymo_collate_fn)
+    #dataset = WaymoDataset(args.tfrecord_dir, args.idx_dir)
+    #dataloader = DataLoader(dataset, batch_size=1, collate_fn=waymo_collate_fn)
 
-    cache_data(dataloader, args.cache_dir)
+    #cache_data(dataloader, args.cache_dir)
+
+    world_size = torch.get_num_threads()
+
+    processes = []
+    for worker_id in range(world_size):
+        p = mp.Process(target=worker, args=(args.tfrecord_dir, args.idx_dir, args.cache_dir, worker_id, world_size))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
