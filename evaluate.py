loss function normalizing is not correct

evaluate.py
@@ -39,9 +39,12 @@ def evaluate(dataloader, model, device):
 
             flow, _ = model(flow_field_times, flow_field_positions, road_map, agent_trajectories, agent_mask)
             
-            flow_field_mask = flow_field_mask.view(-1)
-            flow = flow.view(-1, 2)[flow_field_mask == 1]
-            flow_field_velocities = flow_field_velocities.view(-1, 2)[flow_field_mask == 1]
+            #flow_field_mask = flow_field_mask.view(-1)
+            #flow = flow.view(-1, 2)[flow_field_mask == 1]
+            #flow_field_velocities = flow_field_velocities.view(-1, 2)[flow_field_mask == 1]
+
+            flow = flow[flow_field_mask]
+            flow_field_velocities = flow_field_velocities[flow_field_mask]
 
             world_velocities = get_image_velocity(flow_field_velocities.cpu().numpy())
             world_velocities = torch.from_numpy(world_velocities).to(device)

experiment.py
@@ -70,8 +70,10 @@ def build_model(config, device):
 def prepare_dataset(path, batch_size, is_train=True, distributed=False, rank=0, world_size=1):
     dataset = WaymoCached(path)
 
+    shuffle=is_train
+
     if distributed:
-        sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=is_train, drop_last=is_train)
+        sampler = DistributedSampler(dataset, num_replicas=world_size, rank=rank, shuffle=shuffle, drop_last=is_train)
     else:
         sampler = None
 
@@ -79,7 +81,7 @@ def prepare_dataset(path, batch_size, is_train=True, distributed=False, rank=0, 
         dataset,
         batch_size=batch_size,
         sampler=sampler,
-        shuffle=(sampler is None and is_train),
+        shuffle=(sampler is None and shuffle),
         num_workers=min(batch_size, torch.get_num_threads()),
         collate_fn=waymo_cached_collate_fn,
         pin_memory=True
@@ -159,6 +161,7 @@ def distributed_train(rank, world_size, config, experiment_id):
             if config.logging_enabled and rank==0:
                 wandb.log({'epe': epe})
                 print(f'end point error: {epe}')
+            print(f'end point error: {epe}')
 
         if config.should_visualize:
             visualize(dataloader=test_dataloader, model=model, device=rank, 
@@ -179,17 +182,17 @@ def multi_device_train(config):
 if __name__ == '__main__':
     config = ExperimentConfig(
         data_parallel=True,
-        logging_enabled=True,
-        checkpointing_enabled=True,
+        logging_enabled=False,
+        checkpointing_enabled=False,
         initialize_from_checkpoint=True,
-        should_pre_train=False,#True,
-        should_fine_tune=True,
+        should_pre_train=False,
+        should_fine_tune=False,
         should_evaluate=True,
         should_visualize=False,
         pre_train_path='../data1/waymo_dataset/v1.1/tensor_cache/training',
         fine_tune_path='../data1/waymo_dataset/v1.1/tensor_cache/training',
         test_path='../data1/waymo_dataset/v1.1/tensor_cache/validation',
-        pre_train_batch_size=16,
+        pre_train_batch_size=1,
         pre_train_epochs=100,
         pre_train_lr=1e-4,
         pre_train_weight_decay=0,

model/layers/CrossAttentionTransformer.py
@@ -20,7 +20,7 @@ class CrossAttentionLayer(nn.Module):
         
     def forward(self, query, key, mask=None):
         norm_query = self.norm_query(query)
-        norm_query = norm_query * mask.unsqueeze(-1) if mask is not None else norm_query # TODO: double check this is correct
+        norm_query = norm_query * mask.unsqueeze(-1) if mask is not None else norm_query
         attn_output, _ = self.cross_attention(
             query=norm_query,
             key=key,